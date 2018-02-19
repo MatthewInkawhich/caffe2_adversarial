1fcfd1b48fc0dfc8de8bab1295e2a01f893c0da6
MatthewInkawhich <matthewinkawhich@gmail.com>
2018-02-19 16:29:23 -0500

    corrected bug in AddLeNetModel function;  added AddUpgradedLeNetModel_GPU for gpu support

--- lib/model_defs.py
+++ lib/model_defs.py
@@ -87,7 +87,7 @@ def AddLeNetModel(model, data, num_classes, image_height, image_width, image_cha
     h,w = update_dims(height=h, width=w, kernel=2, stride=2, pad=0)
     # 50 * 4 * 4 stands for dim_out from previous layer multiplied by the image size
     ############# Change dim_in value if images are not 28x28
-    fc3 = brew.fc(model, pool2, 'fc3', dim_in=50 * h * h, dim_out=500)
+    fc3 = brew.fc(model, pool2, 'fc3', dim_in=50 * h * w, dim_out=500)
     fc3 = brew.relu(model, fc3, fc3)
     pred = brew.fc(model, fc3, 'pred', 500, num_classes)
     softmax = brew.softmax(model, pred, 'softmax')
@@ -125,3 +125,37 @@ def AddUpgradedLeNetModel(model, data, num_classes, image_height, image_width, i
     pred = brew.fc(model, fc3, 'pred', 500, num_classes)
     softmax = brew.softmax(model, pred, 'softmax')
     return softmax
+
+
+
+def AddUpgradedLeNetModel_GPU(model, data, num_classes, image_height, image_width, image_channels, device_opts):
+    with core.DeviceScope(device_opts):
+    	# Image size: 64x64
+    	conv1 = brew.conv(model, data, 'conv1', dim_in=image_channels, dim_out=32, kernel=5)
+    	h,w = update_dims(height=image_height, width=image_width, kernel=5, stride=1, pad=0)
+    	# Image size: 60x60
+    	pool1 = brew.max_pool(model, conv1, 'pool1', kernel=2, stride=2)
+    	h,w = update_dims(height=h, width=w, kernel=2, stride=2, pad=0)
+    	relu1 = brew.relu(model, pool1, 'relu1')
+    	# Image size: 30x30
+    	conv2 = brew.conv(model, relu1, 'conv2', dim_in=32, dim_out=64, kernel=5)
+    	h,w = update_dims(height=h, width=w, kernel=5, stride=1, pad=0)
+    	# Image size: 26x26
+    	pool2 = brew.max_pool(model, conv2, 'pool2', kernel=2, stride=2)
+    	h,w = update_dims(height=h, width=w, kernel=2, stride=2, pad=0)
+    	relu2 = brew.relu(model, pool2, 'relu2')
+	# Image size: 13x13
+    	conv3 = brew.conv(model, relu2, 'conv3', dim_in=64, dim_out=64, kernel=5)
+    	h,w = update_dims(height=h, width=w, kernel=5, stride=1, pad=0)
+    	# Image size: 9x9
+    	pool3 = brew.max_pool(model, conv3, 'pool3', kernel=2, stride=2)
+    	h,w = update_dims(height=h, width=w, kernel=2, stride=2, pad=0)
+    	relu3 = brew.relu(model, pool3, 'relu3')
+    	# Image size: 4x4
+    	# 50 * 4 * 4 stands for dim_out from previous layer multiplied by the image size
+    	############# Change dim_in value if images are not 28x28
+    	fc3 = brew.fc(model, relu3, 'fc3', dim_in=64 * h * w, dim_out=500)
+    	fc3 = brew.relu(model, fc3, fc3)
+    	pred = brew.fc(model, fc3, 'pred', 500, num_classes)
+    	softmax = brew.softmax(model, pred, 'softmax')
+    	return softmax
