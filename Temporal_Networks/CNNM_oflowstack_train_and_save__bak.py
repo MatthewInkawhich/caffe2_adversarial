7e3870b802a14000c3d47d3d038099a4a4f43210
MatthewInkawhich <matthewinkawhich@gmail.com>
2018-02-21 13:59:13 -0500

    added gpu support to CNNM_oflowstack_train_and_save.py

--- Temporal_Networks/CNNM_oflowstack_train_and_save__bak.py
+++ Temporal_Networks/CNNM_oflowstack_train_and_save__bak.py
@@ -6,12 +6,12 @@
 # import dependencies
 print "Import Dependencies..."
 from matplotlib import pyplot
-import numpy as np
+import numpy as np 
 import os
 import glob
 import shutil
 import random
-import caffe2.python.predictor.predictor_exporter as pe
+import caffe2.python.predictor.predictor_exporter as pe 
 from caffe2.python import core,model_helper,net_drawer,optimizer,workspace,visualize,brew,utils
 from caffe2.proto import caffe2_pb2
 from caffe2.python.predictor import mobile_exporter
@@ -23,14 +23,11 @@ import JesterDatasetHandler as jdh
 # Gather Inputs
 train_dictionary = os.path.join(os.path.expanduser('~'),"DukeML/datasets/jester/TrainDictionary_5class.txt")
 #train_dictionary = os.path.join(os.path.expanduser('~'),"DukeML/datasets/jester/VerySmallTestDictionary_5class.txt")
-predict_net_out = "CNNM_jester_predict_net.pb" # Note: these are in PWD
-init_net_out = "CNNM_2epoch_jester_init_net.pb"
+predict_net_out = "new_CNNM_jester_predict_net.pb" # Note: these are in PWD
+init_net_out = "new_CNNM_2epoch_jester_init_net.pb"
 checkpoint_iters = 1000
 batch_size = 50
 num_epochs = 2
-
-gpu_no = 0
-device_opts = caffe2_pb2.DeviceOption(device_type=caffe2_pb2.CUDA)
 
 
 ##################################################################################
@@ -43,13 +40,11 @@ print "Entering Main..."
 # Create model helper for use in this script
 
 # specify that input data is stored in NCHW storage order
-arg_scope = {"order":"NCHW", "gpu_id": gpu_no, "use_cudnn": True}
+arg_scope = {"order":"NCHW"}
 
 # create the model object that will be used for the train net
 # This model object contains the network definition and the parameter storage
 train_model = model_helper.ModelHelper(name="CNNM_jester_train", arg_scope=arg_scope)
-train_model.param_init_net.RunAllOnGPU()
-train_model.net.RunAllOnGPU()
 
 ##################################################################################
 #### Add the model definition the the model object
@@ -62,66 +57,66 @@ train_model.net.RunAllOnGPU()
 def Add_CNN_M(model,data):
 
 	# Shape here = 20x100x100
-	with core.DeviceScope(device_opts):
-		##### CONV-1
-		conv1 = brew.conv(model, data, 'conv1', dim_in=20, dim_out=96, kernel=7, stride=2, pad=0)
-		#norm1 = brew.lrn(model, conv1, 'norm1',order = "NCHW")
-		# Shape here = 96x47x47
-		pool1 = brew.max_pool(model, conv1, 'pool1', kernel=2, stride=2)
-		# Shape here = 96x23x23
-		relu1 = brew.relu(model, pool1, 'relu1')
 
-		# Shape here = 96x23x23
+	##### CONV-1
+	conv1 = brew.conv(model, data, 'conv1', dim_in=20, dim_out=96, kernel=7, stride=2, pad=0)
+	#norm1 = brew.lrn(model, conv1, 'norm1',order = "NCHW")
+	# Shape here = 96x47x47
+	pool1 = brew.max_pool(model, conv1, 'pool1', kernel=2, stride=2)
+	# Shape here = 96x23x23
+	relu1 = brew.relu(model, pool1, 'relu1')
 
-		##### CONV-2
-		conv2 = brew.conv(model, 'relu1', 'conv2', dim_in=96, dim_out=256, kernel=5, stride=2, pad=1)
-		# Shape here = 256x11x11
-		pool2 = brew.max_pool(model, conv2, 'pool2', kernel=2, stride=2)
-		# Shape here = 256x5x5
-		relu2 = brew.relu(model, pool2, 'relu2')
+	# Shape here = 96x23x23
 
-		# Shape here = 256x5x5
+	##### CONV-2
+	conv2 = brew.conv(model, 'relu1', 'conv2', dim_in=96, dim_out=256, kernel=5, stride=2, pad=1)
+	# Shape here = 256x11x11
+	pool2 = brew.max_pool(model, conv2, 'pool2', kernel=2, stride=2)
+	# Shape here = 256x5x5
+	relu2 = brew.relu(model, pool2, 'relu2')
 
-		##### CONV-3
-		conv3 = brew.conv(model, 'relu2', 'conv3', dim_in=256, dim_out=512, kernel=3, stride=1, pad=1)
-		# Shape here = 512x5x5
-		relu3 = brew.relu(model, conv3, 'relu3')
+	# Shape here = 256x5x5
 
-		# Shape here = 512x5x5
+	##### CONV-3
+	conv3 = brew.conv(model, 'relu2', 'conv3', dim_in=256, dim_out=512, kernel=3, stride=1, pad=1)
+	# Shape here = 512x5x5
+	relu3 = brew.relu(model, conv3, 'relu3')
 
-		##### CONV-4
-		conv4 = brew.conv(model, 'relu3', 'conv4', dim_in=512, dim_out=512, kernel=3, stride=1, pad=1)
-		# Shape here = 512x5x5
-		relu4 = brew.relu(model, conv4, 'relu4')
+	# Shape here = 512x5x5
 
-		# Shape here = 512x5x5
+	##### CONV-4
+	conv4 = brew.conv(model, 'relu3', 'conv4', dim_in=512, dim_out=512, kernel=3, stride=1, pad=1)
+	# Shape here = 512x5x5
+	relu4 = brew.relu(model, conv4, 'relu4')
 
-		##### CONV-5
-		conv5 = brew.conv(model, 'relu4', 'conv5', dim_in=512, dim_out=512, kernel=3, stride=1, pad=1)
-		# Shape here = 512x5x5
-		pool5 = brew.max_pool(model, conv5, 'pool5', kernel=2, stride=2)
-		# Shape here = 512x2x2
-		relu5 = brew.relu(model, pool5, 'relu5')
+	# Shape here = 512x5x5
 
-		# Shape here = 512x2x2
+	##### CONV-5
+	conv5 = brew.conv(model, 'relu4', 'conv5', dim_in=512, dim_out=512, kernel=3, stride=1, pad=1)
+	# Shape here = 512x5x5
+	pool5 = brew.max_pool(model, conv5, 'pool5', kernel=2, stride=2)
+	# Shape here = 512x2x2
+	relu5 = brew.relu(model, pool5, 'relu5')
 
-		fc6 = brew.fc(model, relu5, 'fc6', dim_in=512*2*2, dim_out=4096)
-		relu6 = brew.relu(model, fc6, 'relu6')
+	# Shape here = 512x2x2
 
-		# Shape here = 1x4096
+	fc6 = brew.fc(model, relu5, 'fc6', dim_in=512*2*2, dim_out=4096)
+	relu6 = brew.relu(model, fc6, 'relu6')
 
-		fc7 = brew.fc(model, relu6, 'fc7', dim_in=4096, dim_out=4096)
-		relu7 = brew.relu(model, fc7, 'relu7')
+	# Shape here = 1x4096
 
-		# Shape here = 1x4096
+	fc7 = brew.fc(model, relu6, 'fc7', dim_in=4096, dim_out=4096)
+	relu7 = brew.relu(model, fc7, 'relu7')
 
-		fc8 = brew.fc(model, relu7, 'fc8', dim_in=4096, dim_out=5)
+	# Shape here = 1x4096
 
-		# Shape here = 1x5
+	fc8 = brew.fc(model, relu7, 'fc8', dim_in=4096, dim_out=5)
 
-		softmax = brew.softmax(model,fc8, 'softmax')
+	# Shape here = 1x5
 
-		return softmax
+	softmax = brew.softmax(model,fc8, 'softmax')
+
+	return softmax
 
 
 # Add the model definition to the model
@@ -131,7 +126,7 @@ softmax=Add_CNN_M(train_model, 'data')
 #### Step 3: Add training operators to the model
 
 ITER = brew.iter(train_model, "iter")
-train_model.Checkpoint([ITER] + train_model.params, [], db="cnnm_checkpoint_%05d.lmdb", db_type="lmdb", every=checkpoint_iters)
+train_model.Checkpoint([ITER] + train_model.params, [], db="new_cnnm_checkpoint_%05d.lmdb", db_type="lmdb", every=checkpoint_iters)
 
 
 xent = train_model.LabelCrossEntropy(['softmax', 'label'], 'xent')
@@ -213,3 +208,6 @@ pyplot.plot(np.array(loss),'b', label='loss')
 pyplot.plot(np.array(accuracy),'r', label='accuracy')
 pyplot.legend(loc='upper right')
 pyplot.show()
+
+
+
